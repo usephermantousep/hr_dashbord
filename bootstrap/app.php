5ebcf03bd0dd5a3eeaedfe96f4b6510421104869
usep <usephermantousep@gmail.com>
2025-01-15 20:06:47 +0700

    set nginx conf

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,5 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies('*');
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+    ->withMiddleware(function (Middleware $middleware) {})
+    ->withExceptions(function (Exceptions $exceptions) {})->create();
